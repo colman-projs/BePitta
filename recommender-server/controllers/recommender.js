3b25e60ae36ef8c4335dfca6c938c00ce4538949
guybenda <email@example.com>
2022-06-12 11:49:01 +0300

    Improved error handling for recommender

--- recommender-server/controllers/recommender.js
+++ recommender-server/controllers/recommender.js
@@ -1,51 +1,50 @@
 const errorHandler = require('../globals').errorHandler;
 
 const Group = require('../models/group');
-const Tag = require('../models/tag');
-const Dish = require('../models/dish');
 const Restaurant = require('../models/restaurant');
-const Client = require('../models/client');
 const { calculateScores } = require('../recommenderUtils');
 
-const recommendation = {
-    dishes: [
-        {
-            id: '629202c76a579f0257f7aadb',
-            match: 0.92,
-            users: [1, 3],
-        },
-    ],
-};
-
 const getRecommendations = async (req, res) => {
-    const group = await Group.findById(req.params.groupId)
-        .populate({
-            path: 'users',
-            populate: [
-                {
+    try {
+        const group = await Group.findById(req.params.groupId)
+            .populate({
+                path: 'users',
+                populate: [
+                    {
+                        path: 'tags',
+                    },
+                    {
+                        path: 'likedDishes',
+                    },
+                ],
+            })
+            .exec();
+
+        if (!group) {
+            return res.status(400).send('Group not found');
+        }
+
+        const restaurant = await Restaurant.findById(group.restaurantId)
+            .populate({
+                path: 'dishes',
+                populate: {
                     path: 'tags',
                 },
-                {
-                    path: 'likedDishes',
-                },
-            ],
-        })
-        .exec();
-    const restaurant = await Restaurant.findById(group.restaurantId)
-        .populate({
-            path: 'dishes',
-            populate: {
-                path: 'tags',
-            },
-        })
-        .exec();
+            })
+            .exec();
 
-    const clients = group.users;
-    const dishes = restaurant.dishes;
+        if (!restaurant) {
+            return res.status(400).send('Restaurant not found');
+        }
 
-    const recommendation = calculateScores(clients, dishes);
+        const clients = group.users;
+        const dishes = restaurant.dishes;
 
-    res.json(recommendation);
+        const recommendation = calculateScores(clients, dishes);
+        res.json(recommendation);
+    } catch (e) {
+        errorHandler(res)(e);
+    }
 };
 
 module.exports = {
